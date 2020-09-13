79b8e9db88d6afc7295f54be2a94437a2f23cff7
Sk8056 <sarangkumar8056@gmail.com>
2020-09-13 15:08:06 +0530

    Object advance

--- ObjectsJs.js
+++ ObjectsJs.js
@@ -7,6 +7,7 @@ var user = {
     courseList: [],
     buyCourse: function (courseName) {
         this.courseList.push(courseName); //here this points to user object coz it is not a regulr function. 
+        console.log(this);
         function sayHi() {
             console.log(this); //here this points to window object coz it is regular function.
         }
@@ -18,10 +19,27 @@ var user = {
     }
 
 };
+// console.log(user.getCourseContent());
 
-console.log(user.getCourseContent());
+// function saySomething() { // this is a regular function so, it is a part of global object that is window object.
+//     console.log("Saying Hello");
+// }
+// saySomething();
 
-function saySomething() { // this is a regular function so, it is a part of global object that is window object.
-    console.log("Saying Hello");
-}
-saySomething();
+
+var User = function(name, age) {
+    this.name = name;
+    this.age = age;
+    this.getCourseCount = function() {
+        console.log(`This is ${this.name} and his age is ${this.age}`);
+        console.log("Line 34" + " " + this);
+        function sayHi() {
+            console.log("insde HI function");
+            console.log("Line 37" + " " + this); //regular function call so this points to windows object
+        }
+        sayHi();
+    };
+};
+
+const currentUser = new User("Sarang", 12);
+currentUser.getCourseCount();
